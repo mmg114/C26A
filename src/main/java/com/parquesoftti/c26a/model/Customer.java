75464fc30b280eb647b2f92d1cf35b3ce30f8cb3
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-08-21 20:29:36 -0500

    Customer

--- src/main/java/com/parquesoftti/c26a/model/Customer.java
+++ src/main/java/com/parquesoftti/c26a/model/Customer.java
@@ -1,15 +1,22 @@
 package com.parquesoftti.c26a.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name = "customers")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long customerId;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
 }
